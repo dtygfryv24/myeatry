29f128c0b8e3ae2785bb6b4390bb400fe28fdda4
Tom <tomwilliams79@proton.me>
2025-09-04 17:05:16 +0100

    last changes

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   allowedDevOrigins: ["*.preview.same-app.com"],
   images: {
     unoptimized: true,
